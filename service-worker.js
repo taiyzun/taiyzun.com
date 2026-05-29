f99e500041d5b96ae75da1bd243848660635572c
Taiyzun <taiyzun@gmail.com>
2026-05-30 05:24:39 +0530

    Update T1000 profile previews

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for taiyzun.com
-const CACHE_NAME = 'taiyzun-core-v15';
+const CACHE_NAME = 'taiyzun-core-v16';
 const RUNTIME_IMAGE_CACHE = 'taiyzun-images-v2';
 const ASSETS_TO_CACHE = [
   '/',
@@ -13,8 +13,7 @@ const ASSETS_TO_CACHE = [
   '/js/mobile-menu.js',
   '/assets/manifest.json',
   '/assets/images/logo.png',
-  '/assets/images/TaiyZunS-TruTh-arTed-square-og.jpg',
-  '/assets/images/TaiyZunS-TruTh-arTed-og.jpg',
+  '/assets/images/taiyzun-t1000-profile-og-20260530.jpg',
   '/assets/icons/favicon-32x32.png',
   '/favicon.ico'
 ];
