74cfedf007d6780ed7ac8873806002cf52074bc8
Taiyzun <taiyzun@gmail.com>
2026-05-30 09:57:22 +0530

    Refine Taiyzun business identity metadata

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for taiyzun.com
-const CACHE_NAME = 'taiyzun-core-v19';
+const CACHE_NAME = 'taiyzun-core-v21';
 const RUNTIME_IMAGE_CACHE = 'taiyzun-images-v2';
 const ASSETS_TO_CACHE = [
   '/',
