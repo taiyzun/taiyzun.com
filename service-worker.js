c0ee8f642bdcb47a3fead07b32b3a58f2f21b482
Taiyzun <taiyzun@gmail.com>
2026-06-21 20:51:00 +0530

    Refresh final cache keys and image placeholders

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for taiyzun.com
-const CACHE_NAME = 'taiyzun-core-v62';
+const CACHE_NAME = 'taiyzun-core-v63';
 const RUNTIME_IMAGE_CACHE = 'taiyzun-images-v2';
 const ASSETS_TO_CACHE = [
   '/',
