371eb0f62d9c317f110c5ea64cc5d721226c955d
Taiyzun <taiyzun@gmail.com>
2026-07-05 09:04:01 +0530

    chore: refresh css cache tokens

--- js/creations-css-loader.js
+++ js/creations-css-loader.js
@@ -1,5 +1,5 @@
 (function loadCreationsCss() {
-  const href = '/css/taiyzun-creations.bundle.min.css?v=20260705b';
+  const href = '/css/taiyzun-creations.bundle.min.css?v=20260705c';
   let loaded = false;
   let timer = 0;
 
